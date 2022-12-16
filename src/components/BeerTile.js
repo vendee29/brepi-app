2a1cdf4aa353d382b2fb08ab0152fc10ee9506a4
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-16 16:44:34 +0100

    adds the complete Tile component

--- src/components/BeerTile.js
+++ src/components/BeerTile.js
@@ -2,6 +2,56 @@ import "./BeerTile.css";
 import { Card } from "antd";
 const { Meta } = Card;
 
-const BeerTile = () => {
-  return <div>BeerTile</div>
+const BeerTile = (props) => {
+  const handleClick = () => {
+    props.onClick(props.beer.id);
+  };
+
+  return (
+    <div className="beer_tile_card" onClick={handleClick}>
+      {props.side === "front" ? (
+        <Card
+          hoverable
+          size="small"
+          style={{
+            width: 350,
+            height: 220,
+          }}
+          cover={
+            <img
+              className="beer_tile_card_img"
+              alt="beer-bottle"
+              src={props.beer?.image_url}
+            />
+          }
+        >
+          <Meta
+            className="meta"
+            title={props.beer?.name}
+            description={props.beer?.tagline}
+          />
+        </Card> //or
+      ) : (
+        <Card
+          hoverable
+          size="small"
+          style={{
+            width: 350,
+            height: 220,
+          }}
+          cover={
+            <div className="beer-description">{props.beer?.description}</div>
+          }
+        >
+          <Meta
+            className="meta"
+            title={props.beer?.name}
+            description={props.beer?.tagline}
+          />
+        </Card>
+      )}
+    </div>
+  );
 };
+
+export default BeerTile;
