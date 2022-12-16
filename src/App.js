a45ee51a91c334edf05ec42bfd4ef0f0ded84fef
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-16 16:43:35 +0100

    adds App logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import './App.css';
+import "./App.css";
+import { Pagination } from "antd";
+import Tiles from "./components/Tiles";
+import { useEffect, useState } from "react";
 
-function App() {
+async function fetchBeers(pageNumber, signal) {
+  const response = await fetch(
+    `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=6`,
+    { signal }
+  );
+  return await response.json();
+}
+
+const App = () => {
+  const [page, setPage] = useState(1);
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetchBeers(page, signal)
+      .then((result) => {
+        setBeers(result);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("cancelled");
+        } else {
+          console.log(err);
+        }
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [page]);
+
+  const changeHandler = (event) => {
+    setPage(event);
+  };
+
   return (
     <div className="App">
+      <h1 className="heading">Brepi</h1>
+      {beers && <Tiles beers={beers}></Tiles>}
+      <Pagination
+        onChange={changeHandler}
+        className="pagination"
+        defaultCurrent={1}
+        total={50}
+      />
     </div>
   );
 }
